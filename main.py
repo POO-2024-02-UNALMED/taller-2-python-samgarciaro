a65f351a448d74c311460d39611c6b2e9324b728
Samuel Garcia Rojas <samgarciaro@unal.edu.co>
2024-11-12 13:48:45 -0500

    super correccion

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 class Asiento:
-    def __init__(self):
-        self.color = None
-        self.precio = 0
-        self.registro = 0
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
 
         def cambiarColor(self, color):
             if color in ['rojo', 'verde', 'amarillo', 'negro', 'blanco']:
                 self.color = color
 
 class Auto:
-    def __init__(self):
-        self.modelo = None
-        self.precio = 0
-        self.asientos = []
-        self.marca = None
+    def __init__(self, modelo, precio, asientos, marca, Motor, registro, cantidadCreados):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
         self.motor = Motor()
-        self.registro = 0
-        cantidadCreados = 0
+        self.registro = registro
+        cantidadCreados = cantidadCreados
 
         def cantidadAsientos(self):
             contador = 0
@@ -35,7 +35,14 @@ class Auto:
             return "Auto original"
 
 class Motor:
-    def __init__(self):
-        self.numeroCilindros = 0
-        self.tipo = None
-        self.registro = 0
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+
+    def asignarTipo(self, tipo):
+        if tipo in ["gasolina", "electrico"]:
+            self.tipo = tipo
