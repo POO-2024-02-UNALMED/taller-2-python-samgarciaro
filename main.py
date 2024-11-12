97ad914810f7d1a9fd986fc2394d363334c6a435
Samuel Garcia Rojas <samgarciaro@unal.edu.co>
2024-11-12 13:58:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
 class Asiento:
-    def __init__(self, color, precio, registro):
+    def _init_(self,color, precio,registro):
         self.color = color
         self.precio = precio
         self.registro = registro
 
-        def cambiarColor(self, color):
-            if color in ['rojo', 'verde', 'amarillo', 'negro', 'blanco']:
-                self.color = color
-
+    def cambiarColor(self, color):
+        if color in ["rojo", "verde", "amarillo", "negro", "blanco"]:
+            self.color = color
 class Auto:
-    def __init__(self, modelo, precio, asientos, marca, Motor, registro, cantidadCreados):
+    def _init_(self,modelo,precio,marca,registro,Motor,cantidadCreados):
         self.modelo = None
         self.precio = 0
         self.marca = None
         cantidadCreados = 0
         self.registro = 0
-        self.motor = Motor
-        self.asientos = asientos
+        self.motor = Motor()
+        self.asientos = []
 
-        def cantidadAsientos(self):
-            contador = 0
-            for i in self.asientos:
+    def cantidadAsientos(self):
+        contador = 0
+        for asiento in self.asientos:
+            if asiento is not None:
                 contador += 1
-            return contador
-        
-        def verificarIntegridad(self):
-            if self.motor.registro != self.registro:
-                return "Las piezas no son originales"
-            
-            for asiento in self.asientos:
-                if isinstance(asiento, Asiento) and asiento.registro != self.registro:
-                    return "Las piezas no son originales"
-            
-            return "Auto original"
+        return contador
 
+    def verificarIntegridad(self):
+        for asiento in self.asientos:
+            if isinstance(asiento, Asiento):
+                if asiento.registro != self.motor.registro or asiento.registro != self.registro:
+                    return "Las piezas no son originales"
+        return "Auto original"
 class Motor:
-    def __init__(self, numeroCilindros, tipo, registro):
+    def _init_(self,numeroCilindros,tipo,registro):
         self.numeroCilindros = 0
         self.tipo = None
         self.registro = 0
